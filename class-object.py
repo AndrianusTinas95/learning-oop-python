5d712a61a231a88d32848d477854c645f534a1aa
andriTinas <andrianustinas.siskom@gmail.com>
2019-06-03 23:45:40 +0700

    class-instance-variable

--- class-object.py
+++ class-object.py
@@ -1,15 +1,19 @@
 class Hero:
-    name = "cai"    
+    # class variable
+    count = 0    
     def __init__(self,name,health,power,armor):
+        # is instance variablr
         self.name   = name
         self.health = health
         self.power  = power
         self.armor  = armor
+        Hero.count  +=1
+
+        print("Membuat hero dengan nama " + name)
 
 hero1   = Hero("sniper",100,10,4)
+print(Hero.count)
 hero2   = Hero("sniper",100,15,1)
+print(Hero.count)
 hero3   = Hero("sniper",1000,100,0)
-
-print(hero1.name)
-print(hero2.health)
-print(hero3.__dict__)
+print(Hero.count)
