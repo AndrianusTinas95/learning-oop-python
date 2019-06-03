14376e300878e45b2d64dd7f62278bf7c6553b20
andriTinas <andrianustinas.siskom@gmail.com>
2019-06-03 23:23:55 +0700

    #1

---
+++ class-object.py
@@ -0,0 +1,20 @@
+class Hero:
+    pass
+
+hero1   = Hero()
+hero2   = Hero()
+hero3   = Hero()
+
+
+hero1.name      ="sniper"
+hero1.health    =100
+
+hero1.name      ="sven"
+hero1.health    =200
+
+hero1.name      ="cai"
+hero1.health    =1000
+
+print(hero1)
+print(hero1.__dict__)
+print(hero1.name)
