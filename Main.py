1007c43d2cdd8bf23d4b6a9418bfdc2a2c90ec25
andriTinas <andrianustinas.siskom@gmail.com>
2019-06-04 23:04:14 +0700

    encapsulasi

--- Main.py
+++ Main.py
@@ -1,19 +1,33 @@
 class Hero :
 
     count = 0
-    __privateCount=0
 
-    def __init__(self,name,health) :
-        self.name   =   name
-        self.health =   health
+    def __init__(self,name,health,power) :
+        self.__name = name
+        self.__health = health
+        self.__power = power
+    
+    # getter
+    def getName(self) :
+        return self.__name
+    
+    def getHealth(self) :
+        return self.__health
+    
+    #setter
+    def attacked(self,attackPower) :
+        self.__health -= attackPower
+    
+    def setPower(self,power) :
+        self.__power = power
 
-        # variable instance private
-        self.__private = "private"
 
-        # variable instance protected
-        self._protected = "protected"
+#awal game
+earthshaker = Hero("earthshaker",50,5)
 
-lina    =   Hero("lina",100)
+#game berjalan
+print(earthshaker.getName())
+print(earthshaker.getHealth())
+earthshaker.attacked(5)
 
-print(lina.__dict__)
-# lina.private = "tes saja"
+print(earthshaker.getHealth())
