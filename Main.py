558d3e8d93809c32fd62d14966e01efc050ec6aa
AndriTinas <andrianustinas.siskom@gmail.com>
2019-06-15 00:24:18 +0700

    save

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
 class Hero :
 
-	def __init__(self,name,health):
-		self.name = name
-		self.health= health
+	__expBase=100
+
+	def __init__(self,name):
+		self.__health_pool= [0,100,200,300,400]
+		self.__attPower_pool =[0,20,50,80,100]
+		self.__armor_pool =[0,2,4,6,8]
+
+		self.__name=name
+		self.__exp =0
+		self.__level=0
 
 	@property
 	def info(self):
@@ -10,24 +17,83 @@ class Hero :
 
 	@info.getter
 	def info(self):
-		print("{} punya health {}".format(self.name,self.health))
+		print("{} punya health {}".format(self.__name,self.__health))
+
+	@property
+	def health_pool(self):
+		pass
+
+	@health_pool.setter
+	def health_pool(self,input):
+		 self.__health_pool=input
+
+	@property
+	def attPower_pool(self):
+		pass
+
+	@attPower_pool.setter
+	def attPower_pool(self,input):
+		self.__attPower_pool= input
+
+	@property
+	def armor_pool(self):
+		pass
+
+	@armor_pool.setter
+	def armor_pool(self,input):
+		self.__armor_pool=input
+
+	@property
+	def gainExp(self):
+		pass
+
+	@gainExp.setter
+	def gainExp(self,input):
+		self.__exp=input
+		if self.__exp >= __expBase :
+			self.levelUp =  self.__exp // __expBase
+			self.__exp %=__expBase
+
+	@property
+	def levelUp(self):
+		pass
+
+	@levelUp.setter
+	def levelUp(self,input):
+		self.__level=input
+		self.__health =self.__health_pool[self.__level]
+		self.__attPower=self.__attPower_poll[self.__level]
+		self.__armor=self.__armor_pool[self.__level]
 
 class Hero_intelligent(Hero):
 	def __init__(self,name):
-		super().__init__(name,100)
+		super().__init__(name)
 
+	#override
+	@property
+	def info(self):
+		pass
+
+	@info.getter
+	def info(self):
+		print('{} tipe {} punya health {}'.format(
+			self.name,'intelligent',self.health
+			)
+		)
 
 class Hero_strength(Hero):
 	def __init__(self,name):
-		super().__init__(name,200)
+		super().__init__(name)
+		self.health_pool=[0,200,400,600,800]
+		self.attPower_pool=[0,10,20,30,40]
+		self.armor_pool =[0,0.5,1,1.5,2]
+		self.levelUp=1
 
-sniper = Hero("sniper",100)
-#axe	= Hero_strength('axe')
+axe	= Hero_strength('axe')
 lina	=Hero_intelligent('lina')
 
 
-#print(sniper.info())
-#print(axe.__dict__)
 
-lina.info
+#lina.info
+axe.info
 
